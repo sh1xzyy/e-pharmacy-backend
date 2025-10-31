24da4a07f560f7d04c92be8fc75f187b7384b42f
sh1xzyy <alekslungol@gmail.com>
2025-10-31 16:24:08 +0300

    fix cors

--- src/services/auth.js
+++ src/services/auth.js
@@ -50,7 +50,8 @@ export const loginUser = async (req, res) => {
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -94,7 +95,8 @@ export const refreshUser = async (req, res) => {
 
   res.cookie("refreshToken", newRefreshToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
